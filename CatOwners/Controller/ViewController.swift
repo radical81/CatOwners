a71a1742a578bf2c1011c2101b0efa21568cda83
RadX <ralobba@gmail.com>
2018-03-03 02:17:46 +1100

    Prepare test run for URL call

--- CatOwners/Controller/ViewController.swift
+++ CatOwners/Controller/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//             testRunData() // For debugging URL call
     }
 
+    func testRunData() {
+        let api = APIManager()
+        api.loadData(PET_SOURCE, completion: didLoadData)
+    }
+    
+    func didLoadData(_ items: [Owner]) {
+        for item in items {
+            print("name = \(item.name!)")
+            print("gender = \(item.gender!)")
+            print("age = \(item.age!)")
+            print("pets = \(item.pets!)")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
