6c5db0972cc39fce4350bf7f9827485f7fa9daeb
RadX <ralobba@gmail.com>
2018-03-03 03:14:13 +1100

    Make comparison non-case-sensitive

--- CatOwners/Helper/PetSortHelper.swift
+++ CatOwners/Helper/PetSortHelper.swift
@@ -14,11 +14,11 @@ class PetSortHelper {
     func filterByGenderAndType(gender: String, kind: String, data: [Owner])->[Pet] {
         var malePets : [Pet] = []
         
-        let maleOwners = data.filter{$0.gender == gender}
+        let maleOwners = data.filter{$0.gender?.lowercased() == gender.lowercased()}
         for m in maleOwners {
             if let mPets : [Pet] = m.pets {
                 for pet in mPets {
-                    if(pet.kind == kind) {
+                    if(pet.kind?.lowercased() == kind.lowercased()) {
                         malePets.append(pet)
                     }
                 }
@@ -30,8 +30,8 @@ class PetSortHelper {
     
     func sortPetsByName(ascending:Bool, data: [Pet])->[Pet] {
         if(ascending == true) {
-            return data.sorted(by: {$0.name! < $1.name!})
+            return data.sorted(by: {$0.name!.lowercased() < $1.name!.lowercased()})
         }
-        return data.sorted(by: {$0.name! > $1.name!})
+        return data.sorted(by: {$0.name!.lowercased() > $1.name!.lowercased()})
     }
 }
